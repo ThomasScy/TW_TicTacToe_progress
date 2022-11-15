97533ba8270c8710dc35047985af18216501a666
zinaida <zinaida.j84@gmail.com>
2022-11-15 10:16:32 +0100

    Beatable algorithm completed and some minor chang

--- coordinates.js
+++ coordinates.js
@@ -1,7 +1,8 @@
-const prompt = require("prompt-sync")({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
+const board = require("./board");
 
 module.exports = {
-  getPlayerMove: function (board, current_player) {
+  getPlayerMove: function (board) {
     /*
         Should return the read coordinates for the tic tac toe board from the terminal.
         The coordinates should be in the format  letter, number where the letter is 
@@ -35,7 +36,7 @@ module.exports = {
         console.log("Sorry I didn't understand, please try again.");
         continue;
       }
-      // check if coordinates are free and returns them 
+      // check if coordinates are free and returns them
       if (this.isFieldFree(freeFields, coordinates)) {
         return coordinates;
       } else {
@@ -54,11 +55,39 @@ module.exports = {
         If the board is full (all spots taken by either X or O) than "None"
         should be returned.
         */
-        let freeFields = this.getFreeFields(board);
-        if (freeFields.length === 0) return "None";
-        let randomIndex = this.getRandomInteger(freeFields.length);
+    let freeFields = this.getFreeFields(board);
+    if (freeFields.length === 0) return "None";
+    let randomIndex = this.getRandomInteger(freeFields.length);
 
     return freeFields[randomIndex];
+  },
+
+  getBeatableAiCoordinates: function (board, current_player) {
+    /*
+        Should return an array of 2 numbers. 
+        Each number should be between 0-2.
+        The chosen number should be only a free coordinate from the board.
+        The chosen coordinate should always stop the other player from winning or
+        maximize the current player's chances to win.
+        If the board is full (all spots taken by either X or O) than "None"
+        should be returned.
+        */
+    let freeFields = this.getFreeFields(board);
+    if (freeFields.length === 0) return "None";
+    let otherPlayer = "";
+    if (current_player === "X" ? (otherPlayer = "O") : (otherPlayer = "X"));
+
+    // check for two in line for current player
+    let coordCurrent = this.getBestCoord(board, current_player);
+    let coordOther = this.getBestCoord(board, otherPlayer);
+
+    if (coordCurrent === "None" && coordOther === "None") {
+      return this.getRandomAiCoordinates(board, current_player);
+    } else if (coordCurrent !== "None") {
+      return coordCurrent;
+    } else if (coordOther !== "None") {
+      return coordOther;
+    }
   },
 
   getUnbeatableAiCoordinates: function (board, current_player) {
@@ -71,9 +100,29 @@ module.exports = {
         If the board is full (all spots taken by either X or O) than "None"
         should be returned.
         */
+
     return [1, 1];
   },
-
+  minmax: function (board, currentPlayer, aiPlayer) {
+    // here comes unbeatable AI algorithm
+  },
+  getBestCoord: function (board, player) {
+    // check for two in line of player
+    let boardLine = this.getBestLine(board, player, 2);
+    // if no two in lines are found take the middle field,
+    // if taken just pick random coordinate
+    if (boardLine.length === 0) {
+      // check for middle of field
+      if (board[1][1] === ".") return [1, 1];
+    } else {
+      // if there are two in line fill the one that is empty
+      for (const coord of boardLine) {
+        if (board[coord[0]][coord[1]] === ".") return coord;
+      }
+    }
+    return "None";
+  },
+  // itarated the board and returns coordinates of all free fields
   getFreeFields: function (board) {
     const freeField = [];
     for (let row = 0; row < board.length; row++) {
@@ -83,7 +132,7 @@ module.exports = {
     }
     return freeField;
   },
-
+  // compares values of all subarrays in fieldList with coordinates
   isFieldFree: function (fieldList, coordinates) {
     for (const field of fieldList) {
       if (field[0] === coordinates[0] && field[1] === coordinates[1])
@@ -91,49 +140,120 @@ module.exports = {
     }
     return false;
   },
-  getRandomInteger: function(maxValue){
+  // returns random integer [0,maxValue) maxValue excluded
+  getRandomInteger: function (maxValue) {
     return Math.floor(Math.random() * maxValue);
-  }
+  },
+  //
+  getBestLine: function (board, player, numSimilars) {
+    let counterPlayer = 0;
+
+    for (let row = 0; row < board.length; row++) {
+      // check horizontally
+      counterPlayer = 0;
+      counterDot = 0;
+      for (let i = 0; i < board[row].length; i++) {
+        if (board[row][i] === player) counterPlayer += 1;
+        if (board[row][i] !== ".") counterDot += 1;
+      }
+      if (counterPlayer === numSimilars && counterPlayer + counterDot === 3) {
+        return [
+          [row, 0],
+          [row, 1],
+          [row, 2],
+        ];
+      }
+
+      // check vertically
+      counterPlayer = 0;
+      counterDot = 0;
+      for (let i = 0; i < board[row].length; i++) {
+        if (board[i][row] === player) counterPlayer += 1;
+        if (board[i][row] === ".") counterDot += 1;
+      }
+      if (counterPlayer === numSimilars && counterPlayer + counterDot === 3) {
+        return [
+          [0, row],
+          [1, row],
+          [2, row],
+        ];
+      }
+
+      // check diagonals R
+      counterPlayer = 0;
+      counterDot = 0;
+      for (let i = 0; i < board[row].length; i++) {
+        if (board[i][i] === player) counterPlayer += 1;
+        if (board[i][i] === ".") counterDot += 1;
+      }
+      if (counterPlayer === numSimilars && counterPlayer + counterDot === 3) {
+        return [
+          [0, 0],
+          [1, 1],
+          [2, 2],
+        ];
+      }
+
+      // check diagonals L
+      counterPlayer = 0;
+      counterDot = 0;
+      for (let i = 0; i < board[row].length; i++) {
+        if (board[i][2 - i] === player) counterPlayer += 1;
+        if (board[i][2 - i] === ".") counterDot += 1;
+      }
+      if (counterPlayer === numSimilars && counterPlayer + counterDot === 3) {
+        return [
+          [0, 2],
+          [1, 1],
+          [2, 0],
+        ];
+      }
+    }
+    return [];
+  },
 };
 
 // run this function to test whether you have correctly implemented the other functions
 function checkCoordinates() {
-//   board_1 = [
-//     ["X", "X", "."],
-//     ["X", ".", "."],
-//     ["X", "X", "."],
-//   ];
-//   console.log(
-//     "It should console.log the coordinates selected by the human player"
-//   );
-//   coordinates = module.exports.getPlayerMove(board_1, "X");
-//   console.log(coordinates);
+  //   board_1 = [
+  //     ["X", "X", "."],
+  //     ["X", ".", "."],
+  //     ["X", "X", "."],
+  //   ];
+  //   console.log(
+  //     "It should console.log the coordinates selected by the human player"
+  //   );
+  //   coordinates = module.exports.getPlayerMove(board_1, "X");
+  //   console.log(coordinates);
 
-  board_2 = [
-    ["O", "O", "."],
-    ["X", "X", "."],
-    ["X", "X", "O"],
-  ];
-  console.log("The console.loged coordinate should be only (0,2) or (1,2)");
-  console.log(module.exports.getRandomAiCoordinates(board_2));
-  console.log("The console.loged coordinate should be only (0,2) or (1,2)");
-  console.log(module.exports.getRandomAiCoordinates(board_2));
-  console.log("The console.loged coordinate should be only (0,2) or (1,2)");
-  console.log(module.exports.getRandomAiCoordinates(board_2));
-
-  board_3 = [
-    ["O", "X", "X"],
-    ["X", "O", "X"],
-    ["X", "O", "X"],
-  ];
-  console.log("The console.loged coordinate should be None");
-  console.log(module.exports.getRandomAiCoordinates(board_3));
-
-  // board_4 = [
-  //   [".", "O", "."],
-  //   ["X", "O", "."],
+  // board_2 = [
+  //   ["O", "O", "."],
+  //   ["X", "X", "."],
   //   ["X", "X", "O"],
   // ];
+  // console.log("The console.loged coordinate should be only (0,2) or (1,2)");
+  // console.log(module.exports.getRandomAiCoordinates(board_2));
+  // console.log("The console.loged coordinate should be only (0,2) or (1,2)");
+  // console.log(module.exports.getRandomAiCoordinates(board_2));
+  // console.log("The console.loged coordinate should be only (0,2) or (1,2)");
+  // console.log(module.exports.getRandomAiCoordinates(board_2));
+
+  // board_3 = [
+  //   ["O", "X", "O"],
+  //   ["O", "O", "X"],
+  //   ["X", "X", "X"],
+  // ];
+
+  // console.log("The console.loged coordinate should be None");
+  // console.log(module.exports.getRandomAiCoordinates(board_3));
+
+  board_4 = [
+    [".", ".", "."],
+    [".", "X", "."],
+    [".", "O", "."],
+  ];
+
+  console.log(module.exports.getBeatableAiCoordinates(board_4, "X"));
   // console.log("The console.loged coordinate should always be (0, 0)");
   // console.log(module.exports.getUnbeatableAiCoordinates(board_4, "X"));
 
