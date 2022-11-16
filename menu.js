b092814622ee63ed7af7ae7dd487762577b45a2b
Thomas S <thomas.scy@gmail.com>
2022-11-16 15:05:30 +0100

    add console.clear, add headline

--- menu.js
+++ menu.js
@@ -12,31 +12,38 @@ module.exports = {
         */
 
         const prompt = require("prompt-sync")();
-        console.log(`Choose one of the following options!`);
-        let startOptions = `
-    1. Human vs Human
-    2. Random AI vs Random AI
-    3. Human vs Random AI
-    4. Human vs Unbeatable AI
-    5. Human vs Beatable AI
-    6. Unbeatable AI vs Unbeatable AI \n`;
-        console.log(startOptions);
+        console.log(`   Choose one of the following options! \n`);
+        let startOptions = ""
+            +"   1. Human vs Human\n" 
+            +"   2. Random AI vs Random AI\n"
+            +"   3. Human vs Random AI\n"
+            +"   4. Human vs Unbeatable AI\n"
+            +"   5. Human vs Beatable AI\n"
+            +"   6. Unbeatable AI vs Unbeatable AI";
+       
+        console.log(startOptions +"\n");
     
-        let userInput = prompt(`Your decision is: `);
-    
-        let validInput = ["1", "2", "3", "4", "5", "6"]
+        let userInput = prompt(`   Your decision is: `);
+
+        //console.clear();
+
+        let validInput = ["1", "2", "3", "4", "5", "6"];
         while (true) {
             if (validInput.includes(userInput)) {
+                //console.clear();
                 return userInput;
             } else if (userInput.toLowerCase() === "quit") {
                 return "quit"
             } else {
-                console.log(`"${userInput}" is no option! Choose one of the following options!`);
-                console.log(startOptions);
+                console.clear();
+                console.log(`   "${userInput}" is no option! \n   Choose one of the following options:\n`);
+                console.log(startOptions +"\n");
                 
-                userInput = prompt();
+                userInput = prompt(` #  Your decision is: `);
+                //console.clear();
             }
-       }
+        }
+        
     }
  }
 
